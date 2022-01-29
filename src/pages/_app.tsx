ede881e556bd14d8e602c4dd28a91818038784fc
Pietro <pietro@palmesi.com>
2022-01-29 13:26:15 +0100

    More Netlify fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,13 @@ import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Web3ContextProvider } from '@/context/web3Context';
 import { StyledEngineProvider } from '@mui/material';
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from '@/utils/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
-      <ChakraProvider theme={theme}>
         <Web3ContextProvider>
           <Component {...pageProps} />
         </Web3ContextProvider>
-      </ChakraProvider>
     </StyledEngineProvider>
   );
 }
