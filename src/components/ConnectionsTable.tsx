4e5ecf1d5e930c81862be21f52a5cd60c247b0cb
Pietro <pietro@palmesi.com>
2022-01-30 10:06:02 +0100

    Change some files to typescript

---
+++ src/components/ConnectionsTable.tsx
@@ -0,0 +1,45 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { is_follower, is_following } from "../context/connections.data";
+import Identicon from 'react-identicons';
+
+interface DataProps {
+    data: [[string, number]];
+}
+
+function ConnectionsTable(props : DataProps) {
+    return (
+        <Box>
+            {props.data.map(([address, opts]) =>
+                <Flex
+                    alignItems='center'
+                    justifyContent='flex-start'
+                    maxW='33em'
+                    gap={0}
+                    key={address}
+                >
+                    <Box>
+                        <Flex minW='4em' alignItems='center' mx={-3}>
+                            <Tooltip label={is_follower(opts) ? 'Is a follower.' : "Isn't a follower."} >
+                                <ChevronRightIcon w={6} h={6} mr={-1} color={
+                                    is_follower(opts) ? 'green.400' : 'gray.400'} /></Tooltip>
+                            <Identicon string={address} size={15} />
+                            <Tooltip label={is_following(opts) ? 'Is being followed.' : "Isn't being followed."} >
+                                <ChevronLeftIcon w={6} h={6} ml={-1} color={
+                                    is_following(opts) ? 'yellow.400' : 'gray.400'} />
+                            </Tooltip>
+                        </Flex>
+                    </Box>
+                    <Box
+                        key={address}
+                        wordBreak='break-all'
+                        fontFamily='monospace'
+                    >
+                        {address}
+                    </Box>
+                </Flex>
+            )}
+        </Box>)
+}
+
+export default ConnectionsTable;
